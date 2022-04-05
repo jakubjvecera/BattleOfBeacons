1006630d8dc9c4a70df579675ea714d2cdfc2617
BuildTools <unconfigured@null.spigotmc.org>
2022-04-05 20:28:06 +0200

    nejakej update

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,8 @@ package main;
 import connectAndRespawn.ConnectAndRespawn;
 import listners.PotionOfLeaping;
 import listners.PotionOfStrength;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.plugin.java.JavaPlugin;
 import workingEntity.Trader;
 
@@ -12,6 +14,8 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         var world = getServer().getWorlds().get(0);
         var serverConnect = new ConnectAndRespawn();
+        this.saveDefaultConfig();
+        Bukkit.setDefaultGameMode(GameMode.SURVIVAL);
 
 //        Listeners
         getServer().getPluginManager().registerEvents(serverConnect, this);

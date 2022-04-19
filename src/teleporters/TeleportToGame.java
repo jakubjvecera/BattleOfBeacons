d8778442a425e1957b9bcefe13e88cef81cdda85
BuildTools <unconfigured@null.spigotmc.org>
2022-04-19 19:54:16 +0200

    nejakej update

--- src/teleporters/TeleportToGame.java
+++ src/teleporters/TeleportToGame.java
@@ -3,17 +3,17 @@ package teleporters;
 import lobby.Lobby;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import teams.Team;
-import teams.Teams;
+import teams.Tym;
+import teams.Tymy;
 
 import java.util.List;
 
 public final class TeleportToGame {
     private final Lobby lobby;
-    private final Teams teams;
+    private final Tymy teams;
     private boolean counting = false;
 
-    public TeleportToGame(Lobby lobby, Teams teams) {
+    public TeleportToGame(Lobby lobby, Tymy teams) {
         this.lobby = lobby;
         this.teams = teams;
     }
@@ -43,7 +43,7 @@ public final class TeleportToGame {
         teams.addBattleTeams(lobbyPls, places.size());
         for (int i = 0; i < teams.numberOfTeams(); i++) {
             Location location = places.get(i);
-            Team team = teams.getTeam(i);
+            Tym team = teams.getTeam(i);
             for (Player player : team.getPs()) {
                 player.teleport(location);
             }
